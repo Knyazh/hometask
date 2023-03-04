2fe69bc6d61172688068c78287554caa15c79434
Hp <qnyazface@maul.ru>
2023-03-04 05:59:37 -0800

    Home Task 1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int azn = 1000;
+Console.WriteLine("Mebleg (AZN) : " + azn);
+float usd = 0.5882f;
+float eur = 0.5544f;
+float rub = 44.4444f;
+
+float totalusdazn = usd * azn;
+Console.WriteLine("ABŞ dolları (USD) :" + totalusdazn);
+
+float totaleurazn = eur * azn;
+Console.WriteLine("Avro (EUR) :" + totaleurazn);
+
+float totalrubazn = rub * azn;
+Console.WriteLine("Rusiya rublu (RUB) :" + totalrubazn);
+
+
